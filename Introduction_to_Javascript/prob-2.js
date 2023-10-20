42c4d2441d272bd1847f42e190fda4274ab71c87
unknown <vardhansparsh46@gmail.com>
2023-10-20 16:31:04 +0530

    Problems of Introduction to Javascript

---
+++ Introduction_to_Javascript/prob-2.js
@@ -0,0 +1,14 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Sparsh Vardhan"
+console.log(name);
+name = "Gyan Vardhan"
+console.log(name);
+name = "Kavita Mishra"
+console.log(name);
